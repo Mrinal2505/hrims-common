695c34d3b73916985143583e20e84e94bc307600
Sarthak <sarthak.hti@gmail.com>
2026-06-04 14:03:13 +0530

    hrims-common

---
+++ src/main/java/com/hti/HrimsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.hti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrimsCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrimsCommonApplication.class, args);
+	}
+
+}
